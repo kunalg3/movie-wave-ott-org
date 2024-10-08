19aef1f6c97869e163351c0418d8a44cbe30642e
kunalg3 <kunalgoyal446@gmail.com>
2024-10-09 00:01:25 +0530

    videoembed component style fix

--- frontend/src/components/VideoEmbed.js
+++ frontend/src/components/VideoEmbed.js
@@ -1,22 +1,27 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import AppBarComponent from './AppBarComponent';
 import Footer from './Footer';
 
 const VideoEmbed = () => {
-  const {id}=useParams();
+  const { id } = useParams();
+
   return (
-    <>
-    <AppBarComponent/>
-    <div style={{ width: '100%', height: '600px' }}>
-      <iframe src={`https://www.2embed.cc/embed/${id}`} width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen='true'></iframe>
+    <div className="flex flex-col min-h-screen">
+      <AppBarComponent />
+      <div className="w-full h-[600px]">
+        <iframe
+          src={`https://www.2embed.cc/embed/${id}`}
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          scrolling="no"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <Footer />
     </div>
-    <Footer/>
-    </>
   );
 };
 
 export default VideoEmbed;
-
-
-
