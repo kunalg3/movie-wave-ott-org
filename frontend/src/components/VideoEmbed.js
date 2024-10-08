79665dde1f2e1f6fe4648bd6ee37e299823148a0
kunalg3 <kunalgoyal446@gmail.com>
2024-10-08 20:31:07 +0530

    added player styles

--- frontend/src/components/VideoEmbed.js
+++ frontend/src/components/VideoEmbed.js
@@ -1,12 +1,18 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import AppBarComponent from './AppBarComponent';
+import Footer from './Footer';
 
 const VideoEmbed = () => {
   const {id}=useParams();
   return (
-    <div style={{ width: '100%', height: '1000px' }}>
+    <>
+    <AppBarComponent/>
+    <div style={{ width: '100%', height: '600px' }}>
       <iframe src={`https://www.2embed.cc/embed/${id}`} width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen='true'></iframe>
     </div>
+    <Footer/>
+    </>
   );
 };
 
