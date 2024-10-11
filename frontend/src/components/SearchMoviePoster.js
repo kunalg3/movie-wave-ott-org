dc9b000e1eaf93f005c05d0ca7cb5a282e955703
kunalg3 <kunalgoyal446@gmail.com>
2024-10-11 14:12:23 +0530

    added new api to search movies

---
+++ frontend/src/components/SearchMoviePoster.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Box, Typography, Button, CardMedia } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+const SearchMoviePoster = ({ movie }) => {
+  const [hovered, setHovered] = useState(false);
+  const navigate=useNavigate();
+
+  const handleClick=()=>{
+    navigate(`/video/${movie.imdbId}`)
+  }
+
+  return (
+    <Box
+      className="movie-poster"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      sx={{ position: 'relative' }}
+    >
+      <CardMedia
+        component="img"
+        image={movie.imageSet.verticalPoster.w720}
+        alt={movie.title}
+        sx={{ height: 300, borderRadius: 2 }}
+      />
+      {hovered && (
+        <Box
+          sx={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+            color: 'white',
+            p: 2,
+            textAlign: 'center',
+          }}
+        >
+          <Typography variant="h6">{movie.Title}</Typography>
+          <Button variant="outlined" sx={{ mt: 1 }} onClick={handleClick}>
+            Click To Play
+          </Button>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default SearchMoviePoster;
--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -4,7 +4,7 @@ import Footer from '../components/Footer';
 import { SearchSharp } from '@mui/icons-material';
 import { TextField, Box, InputAdornment, Button } from '@mui/material';
 import axios from 'axios'; // Ensure axios is correctly imported
-import MoviePoster from '../components/MoviePoster';
+import SearchMoviePoster from '../components/SearchMoviePoster';
 
 function Search() {
   const [searchQuery, setSearchQuery] = useState('');
@@ -15,23 +15,11 @@ function Search() {
 
     if (searchQuery.trim()) {
       console.log('Search Query:', searchQuery);
-
-      // Options for the GET request, including dynamic search query
-      const options = {
-        method: 'GET',
-        url: 'https://advance-movie-api.p.rapidapi.com/api/v1/streamitfree/search',
-        params: { query: searchQuery }, // Use searchQuery as a param
-        headers: {
-          'x-rapidapi-key': '4576227ba0msh90b78f2ee1ae1d7p14ac68jsnc5958ab4d966',
-          'x-rapidapi-host': 'advance-movie-api.p.rapidapi.com',
-        },
-      };
-
       try {
         // Make the GET request to the API
-        const response = await axios.request(options);
-        console.log(response.data.result.data);
-        setSearchResults(response.data.result.data); // Store the results in state
+        const response = await axios.get(`/movie/search?title=${searchQuery}`);
+        console.log(response.data);
+        setSearchResults(response.data); // Store the results in state
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -69,15 +57,11 @@ function Search() {
       </Box>
 
       {/* Display search results */}
-      <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" mt={4}>
-        {searchResults ? (
-          <div>
-            {searchResults.map((movie) => (
-              <div key={movie.Id} className="search-result-item">
-                <MoviePoster key={movie.Id} movie={movie}/>
-              </div>
-            ))}
-          </div>
+      <Box display="flex" justifyContent="space-between" >
+        {searchResults.length>0 ? (
+            searchResults.map((movie) => (
+                <SearchMoviePoster key={movie.Id} movie={movie}/>
+            ))
         ) : (
           <p>No results found</p>
         )}
